e4500e23f15c39ff88e20be52a749ec253c72ad0
Pham Dao Vu <145467039+phmvu2912@users.noreply.github.com>
2023-11-15 23:49:34 +0700

    Update loaiphong.php

--- admin/loaiphong.php
+++ admin/loaiphong.php
@@ -1,39 +1,67 @@
+    <!-- Content -->
+    <main>
+        <div class="top_main">
+            <div class="title">
+                <h1>DANH SÁCH PHÒNG</h1>
+            </div>
 
-    
-    <table border="1">
-        <tr>
-            <th>Tên loại phòng</th>
-            <th>Ảnh</th>
-            <th>Giá</th>
-            <th>Số giường</th>
-            <th>Mô tả</th>
-            <th>Tiện ích</th>
-            <th></th>
-        </tr>
-        <tr>
-            <td>Deluxe Double River View</td>
-            <td><img src="" alt=""></td>
-            <td>1.000.000đ</td>
-            <td>2</td>
-            <td>Phòng cao cấp</td>
-            <td>Điều hòa, Wifi, TV</td>
-            <td>
-                <a href="#">Update</a>
-                <a href="#">Delete</a>
-            </td>
-        </tr>
-        <tr>
-            <td>Premium Gold Suite</td>
-            <td><img src="" alt=""></td>
-            <td>551.250 vnđ</td>
-            <td>2</td>
-            <td>Phòng cao cấp</td>
-            <td>Điều hòa, Wifi, TV</td>
-            <td>
-                <a href="#">Update</a>
-                <a href="#">Delete</a>
-            </td>
-        </tr>
-    </table>
-</body>
-</html>
+            <div class="add">
+                <button type="button"><a href="#">Thêm mới</a></button>
+            </div>
+        </div>
+
+        <div class="main_content">
+            <table>
+                <tr>
+                    <th>Tên loại phòng</th>
+                    <th>Hình</th>
+                    <th>Mô tả</th>
+                    <th>Loại giường</th>
+                    <th>Tiện ích</th>
+                    <th>Tools</th>
+                </tr>
+
+                <tr>
+                    <td>Luxury room</td>
+                    <td>
+                        <img src="" alt="">
+                    </td>
+                    <td>Loại phòng cao cấp</td>
+                    <td>King-size</td>
+                    <td>
+                        <ul>
+                            <li>Wifi free</li>
+                            <li>Lễ tân 24h</li>
+                            <li>Vòi tắm đứng</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <a href="#"><i class="fa-solid fa-pencil"></i></a>
+                        <a href="#"><i class="fa-solid fa-trash red"></i></a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Suite room</td>
+                    <td>
+                        <img src="" alt="">
+                    </td>
+                    <td>Loại phòng hội nghị - rộng rãi cho nhiều người sử dụng</td>
+                    <td>Giường đôi</td>
+                    <td>
+                        <ul>
+                            <li>Wifi free</li>
+                            <li>Lễ tân 24h</li>
+                            <li>Vòi tắm đứng</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <a href="#"><i class="fa-solid fa-pencil"></i></a>
+                        <a href="#"><i class="fa-solid fa-trash red"></i></a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </main>
+    <!-- End Content -->
+</div>
