8c6587f2681045a248bc2c3ac69af8551954ddf0
Pham Dao Vu <145467039+phmvu2912@users.noreply.github.com>
2023-11-15 23:51:59 +0700

    Create menu.php

---
+++ admin/menu.php
@@ -0,0 +1,13 @@
+<div class="grid">
+    <!-- Menu -->
+    <nav>
+        <ul>
+            <li><a href="#">TRANG CHỦ</a></li>
+            <li><a href="index.php?act=khachsan">KHÁCH SẠN</a></li>
+            <li><a href="index.php?act=loaiphong">LOẠI PHÒNG</a></li>
+            <li><a href="#">TÀI KHOẢN</a></li>
+            <li><a href="#">BÌNH LUẬN</a></li>
+            <li><a href="#">THỐNG KÊ</a></li>
+        </ul>
+    </nav>
+    <!-- End Menu -->
