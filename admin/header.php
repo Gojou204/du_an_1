5af3ab096d3c64944ce5b8fa25b30c7270551981
Pham Dao Vu <145467039+phmvu2912@users.noreply.github.com>
2023-11-15 23:47:47 +0700

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -3,23 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>Document</title>
     <link rel="stylesheet" href="../assets/css/admin.css">
+    <script src="https://kit.fontawesome.com/7d1c352b9d.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <header>
-        <div class="admin_logout">
-            <h1>Admin</h1>
-            <button>Log out</button>
-        </div>
-        <hr>
-        <ul>         
-            <li><a href="#">Trang chủ</a></li>
-            <li><a href="#">Loại phòng</a></li>
-            <li><a href="#">Danh sách phòng</a></li>
-            <li><a href="#">Khách hàng</a></li>
-            <li><a href="#">Bình luận</a></li>
-            <li><a href="#">Đặt phòng</a></li>        
-        </ul>
-    </header> 
+    <div class="container">
+        <!-- Header -->
+        <header>
+            <div class="grid">
+                <!-- Logo -->
+                <div class="header">
+                    <b>ADMINITRATOR</b>
+                </div>
+                <!-- End Logo -->
+            </div>
+        </header>
+        <!-- End Header --> 
     
