d9454b2511eceae10f37244bc5e4a0afea351032
Pham Dao Vu <145467039+phmvu2912@users.noreply.github.com>
2023-11-19 20:04:35 +0700

    Add files via upload

---
+++ admin/admin/menu.php
@@ -0,0 +1,13 @@
+<div class="grid">
+    <!-- Menu -->
+    <nav>
+        <ul>
+            <li><a href="index.php">TRANG CHỦ</a></li>
+            <li><a href="index.php?act=khachsan">KHÁCH SẠN</a></li>
+            <li><a href="index.php?act=loaiphong">LOẠI PHÒNG</a></li>
+            <li><a href="index.php?act=taikhoan">TÀI KHOẢN</a></li>
+            <li><a href="index.php?act=danhgia">ĐÁNH GIÁ</a></li>
+            <li><a href="index.php?act=thongke">THỐNG KÊ</a></li>
+        </ul>
+    </nav>
+    <!-- End Menu -->
