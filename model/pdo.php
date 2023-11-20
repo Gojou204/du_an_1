87a694ffdd8c2d3aad8104129bda2665082c29e8
Gojou204 <johnnynguyen2904@gmail.com>
2023-11-20 21:26:22 +0700

    cập nhật pdo

--- model/pdo.php
+++ model/pdo.php
@@ -1,19 +1,69 @@
 <?php
-    function connectdb() {
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-    
-        try {
-            $conn = new PDO("mysql:host=$servername;dbname=du_an_1", $username, $password);
-            // set the PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // echo "Connected successfully";
-        } catch(PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-        }   
-
+// Hàm kết nối đến CSDL sử dụng PDO
+function pdo_get_connection(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=duanmau2023", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
     }
+}
+// Đây là hàm Thực thi câu lệnh sql thao tác dữ liệu như (INSERT, UPDATE, DELETE)
+function pdo_execute($sql){
+    $sql_args=array_slice(func_get_args(),1);
+    try{
+        $conn=pdo_get_connection();
+        $stmt=$conn->prepare($sql);
+        $stmt->execute($sql_args);
 
+    }
+    catch(PDOException $e){
+        throw $e;
+    }
+    finally{
+        unset($conn);
+    }
+}
+// THỰC THI CÂU LỆNH SQL TRUY VẤN KIỂU (SELECT * FROM 'TÊN BẢNG') => về nhiều bản ghi
+
+function pdo_query($sql){
+    $sql_args=array_slice(func_get_args(),1);
+
+    try{
+        $conn=pdo_get_connection();
+        $stmt=$conn->prepare($sql);
+        $stmt->execute($sql_args);
+        $rows=$stmt->fetchAll();
+        return $rows;
+    }
+    catch(PDOException $e){
+        throw $e;
+    }
+    finally{
+        unset($conn);
+    }
+}
+// Thực thi câu lệnh sql truy vấn dữ liệu (SELECT * FROM tên bảng WHERE id/mã/...) => trả về 1 bản ghi 
+function pdo_query_one($sql){
+    $sql_args=array_slice(func_get_args(),1);
+    try{
+        $conn=pdo_get_connection();
+        $stmt=$conn->prepare($sql);
+        $stmt->execute($sql_args);
+        $row=$stmt->fetch(PDO::FETCH_ASSOC);
+        // đọc và hiển thị giá trị trong danh sách trả về
+        return $row;
+    }
+    catch(PDOException $e){
+        throw $e;
+    }
+    finally{
+        unset($conn);
+    }
+}
+pdo_get_connection();
 ?>
