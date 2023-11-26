66d92fd9a05678171d3d7967e966d50158d0518b
Pham Dao Vu <145467039+phmvu2912@users.noreply.github.com>
2023-11-26 20:34:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+    include "model/pdo.php";
+    include "model/khachsan.php";
+    include "model/loaiphong.php";
+    include "model/bed.php";
+    include "global.php";
+
     include "view/header.php";
 
     if(isset($_GET['act']) && ($_GET['act'] != "")) {
@@ -8,9 +14,37 @@
             case 'about':
                 include "view/about.php";
                 break;
+            
             case 'luxury_room' :
                 include "view/chitiet_phong/luxury.php";
                 break;         
+
+            case 'list_hotel' :
+                if(isset($_GET['id_city']) && ($_GET['id_city'])) {
+                    $id_city = $_GET['id_city'];
+
+                    echo $id_city;
+                    getAll_hotel_by_city($id_city);
+                }
+
+                $list_hotel = getAll_hotel();
+                
+                $list_city = getAll_city();
+                include "view/khachsan/list.php";
+                break;
+
+                case 'list_hotel_by_city' :
+                    if(isset($_GET['id_city']) && ($_GET['id_city'])) {
+                        $id_city = $_GET['id_city'];
+    
+                        // echo $id_city;
+                        $list_hotel_by_city = getAll_hotel_by_city($id_city);
+                    }
+    
+                    $list_hotel = getAll_hotel();                   
+                    $list_city = getAll_city();
+                    include "view/khachsan/list_hotel_by_city.php";
+                    break;
         }
         
     }
